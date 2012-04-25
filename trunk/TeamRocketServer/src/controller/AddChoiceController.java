04ed7dc4c0ff2e851f197496f750cdd99da95edd
phantomliger89 <phantomliger89@331b87f8-051d-218c-c15f-add5c7c0d5e5>
2012-04-25 16:20:32 +0000

    added request parsing and response creation. still need to add them to local storage.

--- trunk/TeamRocketServer/src/controller/AddChoiceController.java
+++ trunk/TeamRocketServer/src/controller/AddChoiceController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import model.DLChoice;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+import db.Manager;
+import server.ClientState;
 import xml.Message;
 
 /**
@@ -9,5 +16,32 @@ import xml.Message;
  */
 
 public class AddChoiceController {
+	ClientState state;
+	
+	public AddChoiceController(ClientState cs){
+		this.state = cs;
+	}
+	
+	public Message process(Message request){
+		
+		Node first = request.contents.getFirstChild();
+		NamedNodeMap map = first.getAttributes();
+		String id = map.getNamedItem("id").getNodeValue();
+		String number = map.getNamedItem("number").getNodeValue();
+		String choice = map.getNamedItem("choice").getNodeValue();
+		
+		
+		int choiceNum = Integer.getInteger(number);
+		
+		Manager.insertChoice(id, choiceNum, choice);
+		
+		//add choice to local
+		DLChoice dlc = new DLChoice(choiceNum, choice); 
+		
+		String xml = Message.responseHeader(request.id()) + "<addEdgeResponse id='" + id + "' number='" + number + "' choice='" + choice + "'/></response>";
+		Message response = new Message(xml);
+		
+		return response;
+	}
 
 }
