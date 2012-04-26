904e4a15e8ae6b71c05d977e13d8187cf92ddc78
phantomliger89 <phantomliger89@331b87f8-051d-218c-c15f-add5c7c0d5e5>
2012-04-26 23:51:43 +0000

    Fixed TurnResponseController tested in TestTurnResponseController added addTestDLEvent to model

--- trunk/TeamRocketServer/test/controller/TestTurnResponseController.java
+++ trunk/TeamRocketServer/test/controller/TestTurnResponseController.java
@@ -1,5 +1,15 @@
 package controller;
 
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+import db.Manager;
+
+import java.sql.Date;
+
+import model.DLEvent;
+import model.Edge;
+import xml.Message;
 import junit.framework.TestCase;
 
 /**
@@ -9,8 +19,44 @@ import junit.framework.TestCase;
  */
 
 public class TestTurnResponseController extends TestCase {
+	TurnResponseController cont;
 
-	public void testTemp(){
+	public void setUp(){
+		cont = new TurnResponseController();
+	}
+	public void testTurnProcess(){
+		Message.configure("decisionlines.xsd");
+		DLEvent temp = new DLEvent("test", "tester", "Hello?", 1, 1);
+		Date tempDate = new Date(0);
+		temp.setDateCreated(tempDate);
+		cont.model.addTestDLEvent(temp);
+		
+		Message response = cont.process("test");
+		
+		Node first = response.contents.getFirstChild();
+		NamedNodeMap map = first.getAttributes();
+		String completed = "";
+		if (map.getNamedItem("completed") != null){
+			completed = map.getNamedItem("completed").getNodeValue();
+		}
+		
+		assertEquals("", completed);
+		
+		temp.addEdge(new Edge(1,2,23));
+		
+		response = cont.process("test");
+		
+		first = response.contents.getFirstChild();
+		map = first.getAttributes();
+		completed = "";
+		if (map.getNamedItem("completed") != null){
+			completed = map.getNamedItem("completed").getNodeValue();
+		}
+		
+		assertEquals("true", completed);
+		
+		Manager.deleteEvent("test");
+		
 		
 	}
 }
