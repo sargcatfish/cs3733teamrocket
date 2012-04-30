f9a8bc782106622f9d4c2135a1da63ca13f3d5eb
phantomliger89 <phantomliger89@331b87f8-051d-218c-c15f-add5c7c0d5e5>
2012-04-30 19:08:41 +0000

    javadoc DLChoice ans Edge

--- TeamRocketServer/src/model/Edge.java
+++ TeamRocketServer/src/model/Edge.java
@@ -1,24 +1,50 @@
 package model;
 
+/**
+ * Edge class
+ * @author Timothy Kolek
+ *
+ */
 public class Edge {
+	/** Integer of the number of the left choice of the edge	 */
 	int leftChoice;
+	/** Integer of the number of the right choice of the edge	 */
 	int rightChoice;
+	/** Integer of the number of the height of the edge	 */
 	int height;
 	
+	/**
+	 * Constructor for an Edge
+	 * @param left integer for the left choice
+	 * @param right integer for the right choice
+	 * @param height integer for the height
+	 */
 	public Edge(int left, int right, int height){
 		leftChoice = left;
 		rightChoice = right;
 		this.height = height;
 	}
 
+	/**
+	 * Getter for the left choice
+	 * @return integer of the left choice
+	 */
 	public int getLeftChoice(){
 		return leftChoice;
 	}
 	
+	/**
+	 * Getter for the right choice
+	 * @return integer of the left choice
+	 */
 	public int getRightChoice(){
 		return rightChoice;
 	}
 	
+	/**
+	 * Getter for the height
+	 * @return integer of the left choice
+	 */
 	public int getHeight(){
 		return height;
 	}
