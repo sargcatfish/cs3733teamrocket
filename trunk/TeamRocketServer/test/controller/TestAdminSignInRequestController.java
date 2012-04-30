e6ab42f2221c0fbd95ea089b830243a36d7ad6a1
gdmcconnell@gmail.com <gdmcconnell@gmail.com@331b87f8-051d-218c-c15f-add5c7c0d5e5>
2012-04-30 01:53:42 +0000

    TestAdminSignInRequestController is working now

--- trunk/TeamRocketServer/test/controller/TestAdminSignInRequestController.java
+++ trunk/TeamRocketServer/test/controller/TestAdminSignInRequestController.java
@@ -1,29 +1,56 @@
 package controller;
 
-import model.Admin;
+import org.w3c.dom.NamedNodeMap;
+
 import model.TeamRocketServerModel;
 import xml.Message;
 import junit.framework.TestCase;
+
 /**
  * 
- * @author Ian Lukens, Wesley Nitinthorn
+ * @author Ian Lukens, Wesley Nitinthorn, Greg McConnell
  *
  */
 public class TestAdminSignInRequestController extends TestCase {
 		AdminSignInRequestController adminSignIn;
-		String key ;
 		Message adminMsg ;
 		TeamRocketServerModel server ;
-		String adminStr = "<request version='1.0' id='fdsfrr4'>" +
-				"<adminRequest>" + "<user name='admin' password='pss' />" +
-				"</adminRequest></request>" ;
+		String adminStrFail = Message.requestHeader()+"<adminRequest><user name='admin' password='pss'/></adminRequest></request>";
+		String adminStrPass = Message.requestHeader()+"<adminRequest><user name='admin' password='password'/></adminRequest></request>";
 		
 		public void testWrongPassword(){
+			// setup the system
 			Message.configure("decisionlines.xsd");
-			adminMsg = new Message(this.adminStr);
+			adminMsg = new Message(adminStrFail);
 			adminSignIn = new AdminSignInRequestController(null);
 			
+			// process message
 			Message response = adminSignIn.process(adminMsg);
-			assertFalse(response.success()) ;
-	}
+
+			// retrieve contents
+			NamedNodeMap signInR = response.contents.getAttributes();
+			String reason = signInR.getNamedItem("reason").getNodeValue();
+			String success = signInR.getNamedItem("success").getNodeValue();
+			
+			// check if the expected response and the received are equal
+			assertEquals("Invalid credential", reason);
+			assertEquals("false", success);
+		}
+		
+		public void testCorrectPassword(){
+			// setup the system
+			Message.configure("decisionlines.xsd");
+			adminMsg = new Message(adminStrPass);
+			adminSignIn = new AdminSignInRequestController(null);
+			
+			// process message
+			Message response = adminSignIn.process(adminMsg);
+
+			// retrieve contents
+			NamedNodeMap signInR = response.contents.getAttributes();
+			String success = signInR.getNamedItem("success").getNodeValue();
+			
+			// check if the expected response and the received are equal
+			assertEquals("true", success);
+		}
 }
